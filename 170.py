82e735bd7cf3e4ef3e5dd652cf4a28c64296c0a1
user <teslak70@gmail.com>
2018-12-03 17:59:49 +0200

    20181203_17_59

---
+++ 170.py
@@ -0,0 +1,42 @@
+# fails : 170.py
+# autors : Ints Muraans
+# apliecības numurs : 181REB253
+# datums : 03 12 2018
+# sagatave funkcijas saknes mekleeshanai ar dihatomijas metodi
+
+# -*- coding : tf -8 -*-
+from math import sin, fabs
+from time import sleep
+
+def f(x):
+    return sin(x)
+
+#definejam argumenta x robezhas:
+a = 1.1
+b = 3.2 
+k=0
+# Aprekjinam argumenta x robezhas:
+funa = f(a)
+funb = f(b)
+
+# Paarbaudam, vai dtotajaa intervaalaa ir saknes:
+if (funa * funb > 0.0):
+    print("Dotajaa intervaalaa [%s, %s] saknju nav"%(a,b))
+    sleep(1); exit()    # Zinjo uz ekraana, gaida 1 sec. un darbu pabeidz
+else:
+    print ("Dotajaa intervaalaa sakne(s) ir!")
+
+# Defineejam precizitaati, ar kaadu mekleesim sakni:
+deltax = 0.01
+# sashaurinam saknes mekleeshanas robezhas:
+while (fabs (b-a) > deltax ):
+    x = (a+b)/2; funx = f(x)
+    k = k+1
+    if ( funa*funx < 0. ):
+        b=x
+    else:
+        a=x
+
+print ("Sakne ir: ", x)
+print ("funkcijas vērtība: ", f(x))
+print ("sashaurinaaja tik reizes: ",  k )
---
+++ 171.py
@@ -0,0 +1,39 @@
+# fails : 170.py
+# autors : Ints Muraans
+# apliecības numurs : 181REB253
+# datums : 03 12 2018
+# sagatave funkcijas saknes mekleeshanai ar dihatomijas metodi
+
+# -*- coding : tf -8 -*-
+from math import sinh, fabs
+from time import sleep
+
+def f(x):
+    return sinh(x/2)
+
+#definejam argumenta x robezhas:
+a = 1.1
+b = 3.2 
+
+# Aprekjinam argumenta x robezhas:
+funa = f(a)
+funb = f(b)
+
+# Paarbaudam, vai dtotajaa intervaalaa ir saknes:
+if (funa * funb > 0.0):
+    print("Dotajaa intervaalaa [%s, %s] saknju nav"%(a,b))
+    sleep(1); exit()    # Zinjo uz ekraana, gaida 1 sec. un darbu pabeidz
+else:
+    print ("Dotajaa intervaalaa sakne(s) ir!")
+
+# Defineejam precizitaati, ar kaadu mekleesim sakni:
+deltax = 0.01
+# sashaurinam saknes mekleeshanas robezhas:
+while (fabs (b-a) > deltax ):
+    x = (a+b)/2; funx = f(x)
+    if ( funa*funx < 0. ):
+        b=x
+    else:
+        a=x
+
+print ("Sakne ir: ", x)
